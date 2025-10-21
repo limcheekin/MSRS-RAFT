e9388ac5f524b417ff342a5512f258d154bcc113
limcheekin <limcheekin@vobject.com>
2025-10-21 16:53:16 +0800

    Fix pickling error by disabling multiprocessing in SFTTrainer

--- raft_trainer.py
+++ raft_trainer.py
@@ -370,6 +370,7 @@ class RAFTTrainer:
             eval_dataset=eval_dataset,
             formatting_func=self.formatting_func,
             callbacks=callbacks or [],
+            dataset_num_proc=None,  # Disable multiprocessing to avoid pickling errors with tokenizer
         )
         
         logger.info("Starting training...")
